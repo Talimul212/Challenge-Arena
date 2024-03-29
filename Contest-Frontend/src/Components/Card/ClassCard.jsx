5fdfabfd32cf96e71484430d44d35eaef4e35475
Talimul212 <islamtalimul0@gmail.com>
2024-03-29 23:35:51 +0600

    wip

--- Contest-Frontend/src/Components/Card/ClassCard.jsx
+++ Contest-Frontend/src/Components/Card/ClassCard.jsx
@@ -7,21 +7,47 @@ const ClassCard = () => {
       <CardContainer>
         <Div>
           <p style={{ textTransform: "uppercase", color: "black" }}>TODay</p>
-          <p style={{ color: "black" }}>Thu,28 March</p>
+          <p
+            style={{
+              fontSize: "13px",
+              padding: "3px 6px 3px 6px",
+              borderRadius: "5px",
+              color: "gray",
+              fontWeight: "600",
+              background: "#ffffffc5",
+            }}
+          >
+            Thu, 28 March
+          </p>
         </Div>
         <Div1>
           <BiLike color="gray" size={"40px"} />
-          <p style={{ color: "gray", marginTop: "10px" }}>Thu,28 March</p>
+          <p style={{ color: "gray", marginTop: "10px" }}>
+            Theory of Computation
+          </p>
         </Div1>
       </CardContainer>
       <CardContainer>
         <Div>
-          <p style={{ textTransform: "uppercase", color: "black" }}>TODay</p>
-          <p style={{ color: "black" }}>Thu,28 March</p>
+          <p style={{ textTransform: "uppercase", color: "black" }}>Tomorrow</p>
+          <p
+            style={{
+              fontSize: "13px",
+              padding: "3px 6px 3px 6px",
+              borderRadius: "5px",
+              color: "gray",
+              fontWeight: "600",
+              background: "#ffffffc5",
+            }}
+          >
+            Thu, 28 March
+          </p>
         </Div>
         <Div1>
           <BiLike color="gray" size={"40px"} />
-          <p style={{ color: "gray", marginTop: "10px" }}>Thu,28 March</p>
+          <p style={{ color: "gray", marginTop: "10px" }}>
+            No Classes Tomorrow
+          </p>
         </Div1>
       </CardContainer>
     </Container>
