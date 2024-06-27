55921df42669578621a90adac22d427387a12628
Talimul212 <islamtalimul0@gmail.com>
2024-06-27 12:41:31 +0600

    wip

--- Contest-Frontend/src/helpers/api.js
+++ Contest-Frontend/src/helpers/api.js
@@ -1,2 +1,3 @@
 // export const vercel = "http://localhost:8000";
-export const vercel = "https://university-instructor-backend.vercel.app";
+export const vercel =
+  "https://university-instructor-backend-2do6h4o8v-talimul212s-projects.vercel.app";
